731bc650c7a30fe6fa17cbb5451a78dc3628ed31
Saravanan P <psaravanan.mailme@gmail.com>
2019-07-14 15:35:55 +0530

    Updated for components

--- server/config/helper.js
+++
@@ -1,9 +0,0 @@
-
-// export object
-module.exports = {
-  
-    sendError: function(err, req, res) {
-      res.status(500).send({error: err});
-    }
-  
-  };
--- server/tasks/parentTaskModel.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var parentTaskSchema = new mongoose.Schema({
-    Parent_ID: Number,
-    Parent_Task: String
-});
-
-module.exports = mongoose.model('parentTask', parentTaskSchema);
